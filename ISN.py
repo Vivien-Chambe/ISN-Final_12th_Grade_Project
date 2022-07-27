854a02245a79ca6e5d389359c25f1341c3e0598d
Vivien Chambe <vivien.chambe@live.com>
2022-07-27 12:39:02 +0200

    Adding character movement left and right, add a class Plateform_List to save the group of plateforms,

--- ISN.py
+++ ISN.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import pygame
 from sys import exit
 import random as r
@@ -15,46 +16,89 @@ perso_image = pygame.Surface((50,50))
 
 
 class Character:
-    def __init__(self,x,y,image):
-        self.x = x
-        self.y = y 
-        self.image = image
-        self.test = 0
+    def __init__(self,x,y,w,h):
+        self.body = pygame.Rect(x,y,w,h)
 
-class Platform:
+    def draw(self):
+        pygame.draw.rect(screen,"Red",self.body)
+    
+    def reset_position(self):
+        self.body.x = width/2
+        self.body.y = height-plateform_height-50
+
+class plateform:
     def __init__(self,x,y,w,h):
         self.body = pygame.Rect(x,y,w,h)
 
     def draw(self):
         pygame.draw.rect(screen,"Black",self.body)
 
-platform_width = 50
-platform_height = 20
-nb_platform = 8
-platform_list = []
-space = (height - platform_height - nb_platform*platform_height)/nb_platform
+class Plateform_List:
+    def __init__(self):
+        self.sol = None 
+        self.list = []
+        self.len = 0
+    
+    def draw_level(self):
+        self.sol.draw()
+        for i in range(self.len):
+            self.list[i].draw()
+
+    def restart(self):
+        for i in range(self.len):
+            self.list[i].body.x = r.randint(0,width-plateform_width)
+
+plateform_width = 50
+plateform_height = 20
+nb_plateform = 7
+space = (height - plateform_height - nb_plateform*plateform_height)/nb_plateform
 
 def create_level():
-    sol = Platform(0,height-platform_height,width,platform_height)
-    sol.draw()
+    plateform_list = Plateform_List()
+    sol = plateform(0,height-plateform_height,width,plateform_height)
+    plateform_list.sol = sol
 
-    for i in range (nb_platform):
-        platform_list.append(Platform(r.randint(0,width-platform_width),(sol.body.x)+(i+1)*space+platform_height,platform_width,platform_height))
-        platform_list[i].draw()
+    for i in range (nb_plateform):
+        plateform_list.list.append(plateform(r.randint(0,width-plateform_width),(sol.body.x)+(i+1)*space+plateform_height,plateform_width,plateform_height))
+        plateform_list.len +=1
 
-screen.fill("White")
-create_level()
+    return plateform_list
+
+
+def update_level():
+    screen.fill("White")
+    plateform_list.draw_level()
+    perso.draw()
+
+
+
+plateform_list = create_level()
+
+perso = Character(width/2,height-plateform_height-50,20,50)
+perso.draw()
 
 while True:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
-                platform_list = []
-                screen.fill("White")
-                create_level()
-        
+                plateform_list.restart()
+                perso.reset_position()
+            elif event.key == pygame.K_SPACE:
+                for i in range(200):
+                    perso.body.y -= 1
+                    perso.draw()
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        perso.body.x -= 5
+    elif keys[pygame.K_RIGHT]:
+        perso.body.x += 5
+    
+    update_level()
     pygame.display.update()
     clock.tick(60)
