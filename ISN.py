fc6bf6cd80bce7d8bc5d0eb10200be87907377f9
Vivien Chambe <vivien.chambe@live.com>
2022-08-31 12:10:08 +0200

    Add character sprites, backgrounds, platforms

--- ISN.py
+++ ISN.py
@@ -1,5 +1,6 @@
+from turtle import left
 from unicodedata import name
-import pygame
+import pygame,os,sys
 from sys import exit
 import random as r
 
@@ -12,9 +13,39 @@ pygame.display.set_caption("ISN")
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 50)
 
-perso_image = pygame.Surface((50,50))
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+graphics_dir = resource_path("Graphics")
 fill_surface = pygame.Surface((200,50))
-fill_surface.fill("White")
+
+
+perso_sprite_left_1 =pygame.image.load(graphics_dir + "/sprite_left1.bmp")
+perso_sprite_left_1.set_colorkey("White")
+
+perso_sprite_left_2 =pygame.image.load(graphics_dir + "/sprite_left2.bmp")
+perso_sprite_left_2.set_colorkey("White")
+
+perso_sprite_right_1 =pygame.image.load(graphics_dir + "/sprite_right1.bmp")
+perso_sprite_right_1.set_colorkey("White")
+
+perso_sprite_right_2 =pygame.image.load(graphics_dir + "/sprite_right2.bmp")
+perso_sprite_right_2.set_colorkey("White")
+
+back1 = pygame.image.load(graphics_dir + "/back1.jpg")
+back3 = pygame.image.load(graphics_dir + "/back3.jpg")
+back4 = pygame.image.load(graphics_dir + "/back4.jpg")
+
+flag_sprite = pygame.image.load(graphics_dir + "/flag.jpg")
+flag_sprite.set_colorkey("White")
+
+platforme_sprite = pygame.image.load(graphics_dir + "/platforme.jpg")
+sol_sprite = pygame.image.load(graphics_dir + "/sol.jpg")
+
+backs = [back1,back3,back4]
+
 
 platform_width = 50
 platform_height = 20
@@ -22,21 +53,31 @@ nb_platform = 7
 space = (height - platform_height - nb_platform*platform_height)/nb_platform
 max_height = 180
 
-#colors = list(pygame.colordict.THECOLORS.keys())
-colors = ["White"]
-background_color = r.choice(colors)
-
-
 class Character:
     def __init__(self,x,y,w,h):
         self.body = pygame.Rect(x,y,w,h)
         self.jump_height = 0
         self.falling = False
         self.can_jump = True
+        self.direction = "right"
+        self.frame = 0
+        self.sprite = 1
 
     def draw(self):
-        pygame.draw.rect(screen,"Red",self.body)
-    
+        if self.direction == "right":
+            if self.sprite == 1: screen.blit(perso_sprite_right_1,self.body)
+            elif self.sprite == 2 : screen.blit(perso_sprite_right_2,self.body)
+        elif self.direction == "left":
+            if self.sprite == 1: screen.blit(perso_sprite_left_1,self.body)
+            elif self.sprite == 2 : screen.blit(perso_sprite_left_2,self.body)
+        
+    def update_frame(self):
+        self.frame += 1
+        if self.frame >= 0 and self.frame <50: self.sprite = 1
+        elif self.frame >= 50 and self.frame <100: self.sprite =2
+        elif self.frame >= 100 :self.frame =0
+        else: print("Not a valid frame")
+
     def reset_position(self):
         self.body.x = width/2
         self.body.y = height-platform_height-50
@@ -47,24 +88,27 @@ class Platform:
         self.body = pygame.Rect(x,y,w,h)
         self.color = color
     def draw(self):
-        pygame.draw.rect(screen,self.color,self.body)
+        if self.body.width == width:
+            screen.blit(sol_sprite,self.body)
+        else:
+            screen.blit(platforme_sprite,self.body)
 
 class Platform_List:
     def __init__(self):
         self.sol = Platform(0,height-platform_height,width,platform_height,"Black") 
         self.list = []
         self.len = 0
-        self.flag = Platform(0,0,10,10,"Green")
+        self.flag = Platform(0,0,10,30,"Green")
     
     def draw_level(self):
         self.sol.draw()
         for i in range(self.len):
             self.list[i].draw()
-        self.flag.draw()
+        screen.blit(flag_sprite,self.flag.body)
 
     def restart(self):
-        global background_color
-        background_color = r.choice(colors)
+        global random_background
+        random_background = r.choice(backs)
         for i in range(self.len):
             new_x = r.randint(0,width-platform_width)
             if i>0:
@@ -103,7 +147,7 @@ def create_level():
     return platform_list
 
 def update_level():
-    screen.fill(background_color)
+    screen.blit(random_background,(0,0))
     platform_list.draw_level()
     perso.draw()
     
@@ -139,10 +183,10 @@ def check_collisions(perso,platform,platform_list): #Explanations in the README.
         perso.reset_position()
 
 
-
 platform_list = create_level()
+random_background = r.choice(backs)
 collisions_list = give_rect_for_collision()
-perso = Character(width/2,height-platform_height-50,20,50)
+perso = Character(width/2,height-platform_height-50,30,50)
 perso.draw()
 
 while True:
@@ -163,10 +207,14 @@ while True:
     ############################# Character Movement #########################
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_q]:
         perso.body.x -= 1
-    if keys[pygame.K_RIGHT]:
+        perso.update_frame()
+        perso.direction = "left"
+    if keys[pygame.K_d]:
         perso.body.x += 1
+        perso.update_frame()
+        perso.direction = "right"
 
     if keys[pygame.K_SPACE] and (not perso.falling) and perso.can_jump:
         perso.body.bottom -= 1
